0098b54be38c4957344e5505e8a8cc67f2dd76f1
twtseng <twtseng@users.noreply.github.com>
2020-09-22 16:27:46 -0700

    Add drag and drop

--- server/controllers/board.controller.js
+++ server/controllers/board.controller.js
@@ -10,7 +10,10 @@ module.exports = {
         await Board.find({}, (err,result) => handleResponse(err,result,resp));
     },
     getSingleBoard : async (req,resp) => {
-        await Board.findOne({_id:req.params.id}, (err,result) => handleResponse(err,result,resp));
+        await Board.findOne({_id:req.params.id}, (err,result) => handleResponse(err,result,resp))
+        .populate({ path: 'columns',
+        populate: { path: 'tasks'}
+    });
     },
     updateBoard : async (req,resp) => {
         await Board.findOneAndUpdate({_id:req.params.id}, req.body, {new:true,runValidators:true}, (err,result) => handleResponse(err,result,resp));
